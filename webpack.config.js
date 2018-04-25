b3b76d38ff93297f920faab301e4173cfabd617b
Матвей <mruk@tugush.com>
2018-04-25 22:17:41 +0300

    i faild to use webpack properly =(( but i should commit what i have done

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,43 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+PATHS = {
+    src: path.resolve(__dirname, 'src'),
+    build: path.resolve(__dirname, 'build')
+};
 
 module.exports = {
-    entry: './src/js/index.js',
+    entry: PATHS.src + '/js/index.js',
     output: {
+        path: PATHS.build,
         filename: 'main.js',
-        path: path.resolve(__dirname, './build'),
-        publicPath: './build/'
+        publicPath: './build'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: PATHS.src + '/index.pug'
+        })
+    ],
+    devServer: {
+        contentBase: './build'
+    },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+
+            use: [{
+                loader: 'style-loader'
+            }, {
+                loader: 'css-loader'
+            }, {
+                loader: 'sass-loader'
+            }]
+        }, {
+            test: /\.pug$/,
+            loader: 'pug-loader',
+            options: {
+                pretty: true
+            }
+        }]
     }
 };
