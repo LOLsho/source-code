f44efcd57ee0ee57084116a43cf1e69e826740ef
Матвей <mruk@tugush.com>
2018-04-27 08:32:14 +0300

    added seperation webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const pug = require('./webpack/pug');
+const devserver = require('./webpack/devserver');
+
 
 PATHS = {
     src: path.resolve(__dirname, 'src'),
     build: path.resolve(__dirname, 'build')
 };
 
-module.exports = {
+
+const common = {
     entry: PATHS.src + '/js/index.js',
     output: {
         path: PATHS.build,
@@ -16,28 +21,50 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: PATHS.src + '/index.pug'
         })
-    ],
-    devServer: {
-        // stats: 'errors-only',
-        port: 9000
-    },
-    module: {
-        rules: [{
-            test: /\.scss$/,
-
-            use: [{
-                loader: 'style-loader'
-            }, {
-                loader: 'css-loader'
-            }, {
-                loader: 'sass-loader'
-            }]
-        }, {
-            test: /\.pug$/,
-            loader: 'pug-loader',
-            options: {
-                pretty: true
-            }
-        }]
-    }
+    ]
 };
+
+module.exports = function() {
+    return merge([
+        common,
+        pug(),
+        devserver()
+    ]);
+};
+
+
+// module.exports = {
+//     entry: PATHS.src + '/js/index.js',
+//     output: {
+//         path: PATHS.build,
+//         filename: 'main.js'
+//     },
+//     plugins: [
+//         new HtmlWebpackPlugin({
+//             template: PATHS.src + '/index.pug'
+//         })
+//     ],
+//     devServer: {
+//         // stats: 'errors-only',
+//         port: 9000
+//     },
+//     module: {
+//         rules: [{
+//             test: /\.scss$/,
+//
+//             use: [{
+//                 loader: 'style-loader'
+//             }, {
+//                 loader: 'css-loader'
+//             }, {
+//                 loader: 'sass-loader'
+//             }]
+//         }, {
+//             test: /\.pug$/,
+//             loader: 'pug-loader',
+//             options: {
+//                 pretty: true
+//             }
+//         }]
+//     }
+// };
---
+++ webpack/devserver.js
@@ -0,0 +1,8 @@
+module.exports = function () {
+    return {
+        devServer: {
+            // stats: 'error-only',
+            port: 9000
+        }
+    }
+};
